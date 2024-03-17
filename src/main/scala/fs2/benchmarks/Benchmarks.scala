90fa71996bcd2ee090865a25956baa1487083b6c
Kamil Kloch <kamil.kloch@gmail.com>
2024-03-17 21:18:44 +0100

    Updated benchmarks.

--- src/main/scala/fs2/benchmarks/Benchmarks.scala
+++ src/main/scala/fs2/benchmarks/Benchmarks.scala
@@ -8,54 +8,47 @@ import org.openjdk.jmh.annotations.*
 import java.util.concurrent.TimeUnit
 
 @State(Scope.Thread)
-//@Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
-//@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+//@Warmup(iterations = 1, time = 10, timeUnit = TimeUnit.SECONDS)
+//@Measurement(iterations = 1, time = 10, timeUnit = TimeUnit.SECONDS)
 //@Fork(value = 1, warmups = 1)
 class Benchmarks {
 
   @Benchmark
-  def streamParJoinUnbounded(): Int = {
-    val each = Stream
-      .range(0, 1000)
-      .map(i => Stream.eval(IO.pure(i)))
-    each.parJoin(Int.MaxValue).compile.last.unsafeRunSync().get
+  def streamParJoinUnbounded(): Unit = {
+    val list = List.fill(1024)(Stream.eval(IO.unit))
+    Stream.emits(list).parJoinUnbounded.compile.last.unsafeRunSync().get
   }
 
   @Benchmark
-  def listParJoinUnbounded(): Int = {
-    val each = List
-      .range(0, 1000)
-      .map(i => Stream.eval(IO.pure(i)))
-    each.parJoinUnbounded.compile.last.unsafeRunSync().get
+  def listParJoinUnbounded(): Unit = {
+    val list = List.fill(1024)(Stream.eval(IO.unit))
+    list.parJoinUnbounded.compile.last.unsafeRunSync().get
   }
 
   @Benchmark
   def merge2(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(100)
-    val s2 = Stream.emits(List.fill(5)(0)).covary[IO].repeatN(200)
+    val s = Stream.eval(IO.pure(0)).repeatN(1024)
 
-    s1.merge(s2).compile.last.unsafeRunSync().get
+    s.merge(s).compile.last.unsafeRunSync().get
   }
 
   @Benchmark
   def listParJoin2(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(100)
-    val s2 = Stream.emits(List.fill(5)(0)).covary[IO].repeatN(200)
+    val s = Stream.eval(IO.pure(0)).repeatN(1024)
 
-    List(s1, s2).parJoinUnbounded.compile.last.unsafeRunSync().get
+    List(s, s).parJoinUnbounded.compile.last.unsafeRunSync().get
   }
 
   @Benchmark
   def streamParJoin2(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(100)
-    val s2 = Stream.emits(List.fill(5)(0)).covary[IO].repeatN(200)
+    val s = Stream.eval(IO.pure(0)).repeatN(1024)
 
-    Stream(s1, s2).parJoinUnbounded.compile.last.unsafeRunSync().get
+    Stream(s, s).parJoinUnbounded.compile.last.unsafeRunSync().get
   }
 
   @Benchmark
   def mergeWithEmpty(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(1000)
+    val s1 = Stream.eval(IO.pure(0)).repeatN(1024)
     val s2 = Stream.empty
 
     s1.merge(s2).compile.last.unsafeRunSync().get
@@ -63,7 +56,7 @@ class Benchmarks {
 
   @Benchmark
   def streamParJoinWithEmpty(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(1000)
+    val s1 = Stream.eval(IO.pure(0)).repeatN(1024)
     val s2 = Stream.empty
 
     Stream(s1, s2).parJoinUnbounded.compile.last.unsafeRunSync().get
@@ -71,7 +64,7 @@ class Benchmarks {
 
   @Benchmark
   def listParJoinWithEmpty(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(1000)
+    val s1 = Stream.eval(IO.pure(0)).repeatN(1024)
     val s2 = Stream.empty
 
     List(s1, s2).parJoinUnbounded.compile.last.unsafeRunSync().get
@@ -79,9 +72,9 @@ class Benchmarks {
 
   @Benchmark
   def noopJoinWithEmpty(): Int = {
-    val s1 = Stream.emits(List.fill(10)(0)).covary[IO].repeatN(1000)
+    val s1 = Stream.eval(IO.pure(0)).repeatN(1024)
 
     s1.compile.last.unsafeRunSync().get
   }
-  
+
 }
